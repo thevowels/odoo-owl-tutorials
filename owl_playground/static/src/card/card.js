babadd76b42100c754bb7da15578005eaead92e1
thevowels <aungphyohtet1@gmail.com>
2023-08-03 04:41:12 +0630

    11. Extensive props validation

--- owl_playground/static/src/card/card.js
+++ owl_playground/static/src/card/card.js
@@ -4,4 +4,14 @@ import {Component } from "@odoo/owl"
 
 export class Card extends Component{
 	static template = "owl_playground.card"
+
+	static props = {
+		slots: {
+			type: Object,
+			shape:{
+				title: {type: Object, optional: true},
+				default : Object
+			}
+		}
+	}
 }
