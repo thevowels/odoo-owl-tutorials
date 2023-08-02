9fbd77cd38d9277cd301993ff806a4f1ffba3cc1
thevowels <aungphyohtet1@gmail.com>
2023-08-03 02:02:42 +0630

    4. Props validation

---
+++ owl_playground/static/src/todo/todo.js
@@ -0,0 +1,19 @@
+/** @odoo-module **/
+
+import {Component, useState } from "@odoo/owl"
+
+export class Todo extends Component{
+	static template = "owl_playground.todo"
+
+	static props = {
+		todo : {
+			type: Object,
+			shape: {
+					id : Number,
+					description: { type: String},
+					done: Boolean
+			}
+		}
+
+	}
+}
