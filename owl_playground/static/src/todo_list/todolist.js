5fcf25dfe74b3509111bdd7ddc5007b88060b07d
thevowels <aungphyohtet1@gmail.com>
2023-08-03 03:04:42 +0630

    7. Focusing the input

--- owl_playground/static/src/todo_list/todolist.js
+++ owl_playground/static/src/todo_list/todolist.js
@@ -1,18 +1,33 @@
 /** @odoo-module **/
 
 import {Component, useState} from "@odoo/owl"
-import { Todo } from "../todo/todo" 
+import { Todo } from "../todo/todo"
+import {useAutofocus} from "../utils"
+
 
 export class TodoList extends Component{
+
+
 	static template = "owl_playground.todoList"
 
 	static components = {Todo}
 
-	state = useState({ 
-		todos: [
-				{id: 3, description:'do smth', done:true},
-				{id: 4, description:'id 4', done:false},
-				{id: 10, description:'10th element', done:true}
-				]})
+	setup(){
+		useAutofocus("todoinput")
+
+		this.todoList = useState(
+			 [		{id: 1, description:'do smth', done:true},
+					{id: 2, description:'id 4', done:false},
+					{id: 3, description:'10th element', done:true}
+					])
+		this.nextId = 4
+	}
+
+	addTodo(ev){
+		if(ev.keyCode === 13 && ev.target.value != ""){
+			this.todoList.push({id:this.nextId++, description:ev.target.value, done:false})
+			ev.target.value = "";
+		}
+	}
 
 }
