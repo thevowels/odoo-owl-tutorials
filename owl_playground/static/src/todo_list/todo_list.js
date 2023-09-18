85c769cb88bdfc6036033b73783a043cfa69e4ad
thevowels <aungphyohtet1@gmail.com>
2023-09-19 03:32:06 +0630

    finish 1.6

--- owl_playground/static/src/todo_list/todo_list.js
+++ owl_playground/static/src/todo_list/todo_list.js
@@ -1,17 +1,20 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl"
+import { Component,useState } from "@odoo/owl"
 import { Todo } from "../todo/todo"
 
 export class TodoList extends Component{
     static template="owl_playground.TodoList"
     static components = {Todo}
     setup(){
-        this.todoList = [
-            { id: 3, description: "buy milk", done: false },
-            { id: 4, description: "buy eggs", done: true },
-            { id: 5, description: "buy avocado", done: true },
-        ];
+        this.nextId=0
+        this.todoList=useState([])
+    }
+    addTodo(ev){
+        if(ev.keyCode === 13 && ev.target.value != ""){
+            this.todoList.push({id:this.nextId++, description:ev.target.value, done:false})
+            ev.target.value=""
+        }
     }
 
 }
