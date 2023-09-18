b071e973ac6c9b9f98f79b3e2cd712f53f1979b1
thevowels <aungphyohtet1@gmail.com>
2023-09-19 02:32:23 +0630

    Ex 3 Done

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -1,9 +1,17 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 import { Counter } from "./counter/counter"
+import { Todo } from "./todo/todo"
+
 
 export class Playground extends Component {
     static template = "owl_playground.playground";
-    static components = { Counter }
+    static components = { Counter, Todo }
+
+    setup(){
+        this.state = useState(
+            {todo : {id:3, description:"buy milk",done:true}}
+        )
+    }
 }
