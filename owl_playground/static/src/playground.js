52ba94459a10da086c586cd9f56f9b91c68d4e5f
thevowels <aungphyohtet1@gmail.com>
2023-09-19 01:34:45 +0630

    Owl tutorial ex1

--- owl_playground/static/src/playground.js
+++ owl_playground/static/src/playground.js
@@ -1,7 +1,15 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useState } from "@odoo/owl";
 
 export class Playground extends Component {
     static template = "owl_playground.playground";
+
+    setup(){
+        this.state = useState({value: 0})
+    }
+
+    increment(){
+        this.state.value++
+    }
 }
