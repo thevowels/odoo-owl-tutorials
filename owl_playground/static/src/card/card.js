38b2fe95f121e26689ba809a7ee228e707318971
thevowels <aungphyohtet1@gmail.com>
2023-09-21 15:10:23 +0630

    Finish Chapter 1

---
+++ owl_playground/static/src/card/card.js
@@ -0,0 +1,20 @@
+/** @odoo-module **/
+import { Component } from "@odoo/owl"
+
+export class Card extends Component{
+    static template = "owl_playground.Card"
+}
+
+Card.props = {
+    slots: {
+        type: Object,
+        shape: {
+            default: Object,
+            title: {
+                type: Object,
+                optional: true
+            }
+        }
+
+    }
+}
