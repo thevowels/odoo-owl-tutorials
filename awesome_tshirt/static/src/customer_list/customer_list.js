1caa96df8573afa5bed638773e4a47343003ba82
thevowels <aungphyohtet1@gmail.com>
2023-10-27 17:14:21 +0630

    load and display data 5.3

--- awesome_tshirt/static/src/customer_list/customer_list.js
+++ awesome_tshirt/static/src/customer_list/customer_list.js
@@ -1,8 +1,16 @@
 /** @odoo-module */
+import { useService } from "@web/core/utils/hooks";
 
-const { Component } = owl;
+const { Component, onWillStart } = owl;
 
-export class CustomerList extends Component {}
+export class CustomerList extends Component {
+    setup(){
+        this.orm = useService("orm");
+        onWillStart(async ()=>{
+            this.partners = await this.orm.searchRead("res.partner", [], ["display_name"])
+        })
+    }
+}
 
 CustomerList.template = "awesome_tshirt.CustomerList";
 
