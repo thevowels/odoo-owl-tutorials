a3dc3442ea468ca97202fe2b31c1418d977bd5c5
thevowels <aungphyohtet1@gmail.com>
2023-10-27 17:43:04 +0630

    Adding Search function, work for both active filterd and none

--- awesome_tshirt/static/src/customer_list/customer_list.js
+++ awesome_tshirt/static/src/customer_list/customer_list.js
@@ -1,6 +1,7 @@
 /** @odoo-module */
 import { useService } from "@web/core/utils/hooks";
 import { KeepLast } from "@web/core/utils/concurrency";
+import { fuzzyLookup } from "@web/core/utils/search";
 
 
 const { Component, onWillStart, useState } = owl;
@@ -9,10 +10,13 @@ export class CustomerList extends Component {
     setup(){
         this.orm = useService("orm");
         this.partners = useState({data:[]});
+        this.displayedPartners = useState({data:[]})
         this.domain=[];
         this.keepLast = new KeepLast();
+        this.filterName = "";
         onWillStart(async ()=>{
             this.partners.data = await this.loadCustomers();
+            this.displayedPartners.data =this.partners.data;
         })
     }
 
@@ -21,10 +25,27 @@ export class CustomerList extends Component {
         const checked = ev.target.checked;
         this.domain = checked ? [["has_active_order", "=", true]] : [];
         this.partners.data = await this.keepLast.add(this.loadCustomers());
+        this.filterCustomers(this.filterName);
     }
 
     loadCustomers(){
         return this.orm.searchRead("res.partner", this.domain, ["display_name"])
+    }
+
+    onCustomerFilter(ev){
+        this.filterName = ev.target.value;
+        this.filterCustomers(ev.target.value);
+    }
+    
+    filterCustomers(name){
+        if(name){
+            this.displayedPartners.data = fuzzyLookup(name,
+                                            this.partners.data,
+                                            (partner) => partner.display_name)
+            
+        }else{
+            this.displayedPartners.data = this.partners.data;
+        }
     }
 }
 
